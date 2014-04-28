59811ec83f5317642b7fc8e6d1226953310b7c9d
Mike Crittenden <mikecrittenden@gmail.com>
2014-04-28 18:01:44 -0400

    Add timesheet parsing functionality

--- js/timesheets.js
+++ js/timesheets.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
   if ($('#timesheet_grid').length < 1) {
     return;
   }
+
   $('#timesheet_grid').after('<div id="p2_sidebar"></div>')
   $('#timesheet_grid').after('<div id="p2_content"></div>')
 
+  addFillerSidebar();
+  addPreviewButton();
+  parseTimeGrid();
+});
+
+function addFillerSidebar() {
   var fillerSidebar = '<div>';
-  
   fillerSidebar += '<h2>Your bookings this week.</h2>';
   fillerSidebar += '<div class="sidebar-inner">';
   fillerSidebar += '<table>';
@@ -26,6 +32,9 @@ $(document).ready(function() {
   fillerSidebar += '</div></div>';
 
   $('#p2_sidebar').append(fillerSidebar);
+}
+
+function addPreviewButton() {
   $('#timesheet_savebutton').insertBefore('#save_grid_submit');
   $('<button id="p2_preview" class="btn-oa">Preview</button>').insertBefore('#save_grid_submit');
 
@@ -37,5 +46,44 @@ $(document).ready(function() {
     } else {
       $('#p2_preview').html('Preview');
     }
-  })
-});
+  });
+}
+
+function parseTimeGrid() {
+  var p2_projects = [];
+  $('.timesheetControlPopupCustomerProject option').each(function() {
+    if ($(this).text().length > 0) {
+      p2_projects[$(this).val()] = $(this).text();
+    }
+  });
+  console.log(p2_projects);
+
+  var p2_time = [];
+  $('.timesheetHours').each(function() {
+    time = $(this).find('.timesheetInputHour').val();
+    if (time.length < 1) {
+      return;
+    }
+    date = $(this).find('a').attr('data-additional-title');
+    date = date.substring(0, 2);
+    project = $(this).parents('tr').find('.timesheetControlPopupCustomerProject').val();
+    projectName = p2_projects[project];
+    task = $(this).parents('tr').find('.timesheetControlPopup').val();
+    taskName = $(this).parents('tr').find('.timesheetControlPopup option:selected').text();
+    notesID = $(this).find('a').attr('data-additional-prefix');
+    notes = $('input[name=' + notesID + '_dialog_notes]').val();
+    p2_time.push(
+      {
+        time: time,
+        date: date,
+        project: project,
+        projectName: projectName,
+        task: task,
+        taskName: taskName,
+        notes: notes
+      }
+    );
+  });
+
+  console.log(p2_time)
+}
