fcc8b8ca517582d67cbb72c1fce896297a267c84
Mike Crittenden <mikecrittenden@gmail.com>
2014-04-28 16:16:05 -0400

    add regions

--- js/timesheets.js
+++ js/timesheets.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-  alert('Hi! The extension is working!');
+  if ($('#timesheet_grid').length < 1) {
+    return;
+  }
+  $('#timesheet_grid').after('<div id="p2_sidebar">This is the sidebar</div>')
+  $('#timesheet_grid').after('<div id="p2_content">This is the content</div>')
 });
