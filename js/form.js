49c001ab407eb97f7a6851406fee0b82817e248b
Jace Poirier-Pinto <jacepoirierpinto@gmail.com>
2014-04-28 17:59:06 -0400

    Placement and styling of form (no functionality)

--- js/form.js
+++ js/form.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
 
   var $form = $('<div class="p2_form-wrapper"></div>');
-  $form.append('<div class="p2_form-input_group"><label for="p2_working_on">What are you working on?</label><input type="text" id="p2_working_on" name="p2_working_on" /></div>');
-  $form.append('<div class="p2_form-input_group"<label for="p2_which_client">Which Client?</label><input type="text" id="p2_which_client" name"p2_which_client" /></div>');
-  $form.append('<div class="p2_form-input_group"<label for="p2_which_task">Which Task?</label><input type="text" id="p2_which_task" name"p2_which_task" /></div>');
-  $form.append('<div class="p2_form-input_group"<label for="p2_how_long">How Long?</label><input type="text" id="p2_how_long" name"p2_how_long" /></div>');
-  $form.append('<div class="p2_form-submit_button"><input type="submit" value="Start" /></div>');
+  var $row = $('<div class="p2_form-row"></div>');
+  var $rowTwo = $('<div class="p2_form-row"></div>');
 
-  $form.appendTo('#timesheet_grid');
+  $row.append('<div class="p2_form-input_group p2_one-whole"><label class="p2_form-label" for="p2_working_on">What are you working on?</label><input type="text" id="p2_working_on" name="p2_working_on" class="p2_input-field" /></div>');
+
+  $rowTwo.append('<div class="p2_form-input_group p2_one-third"><label class="p2_form-label" for="p2_which_client">Which Client?</label><input type="text" id="p2_which_client" name"p2_which_client" class="p2_input-field" /></div>');
+  $rowTwo.append('<div class="p2_form-input_group p2_one-third"><label class="p2_form-label" for="p2_which_task">Which Task?</label><input type="text" id="p2_which_task" name"p2_which_task" class="p2_input-field" /></div>');
+  $rowTwo.append('<div class="p2_form-input_group p2_one-third"><label class="p2_form-label" for="p2_how_long">How Long?</label><input type="text" id="p2_how_long" name"p2_how_long" class="p2_input-field" /><div class="p2_form-submit_button"><input type="submit" value="Start" /></div></div>');
+
+  $form
+    .append($row)
+    .append($rowTwo);
+
+  $form.prependTo('#p2_content');
 
 });
