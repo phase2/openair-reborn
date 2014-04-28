ad16ef743540c7180a018cb7e8bcf89c87c7c33e
Jake Love <jlove@Jakes-MacBook-Pro.local>
2014-04-28 16:45:57 -0400

    add some css structure and a splash of style

--- js/table.js
+++ js/table.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	var newTimeTable = "<div class='p2TimeTableWrapper'><h1>DAY NAME</h1><table><tr><td class='p2timeWorkingOn'>Client Work, Other Stuff, etc</td><td class='p2WhichClient'>Turner NBA Platform Build</td><td class='p2ProjectStatus'>Sprint 17</td><td class='p2TimeSpent'>1.25 h</td><td class='p2EditThis'><a href='#'>EDIT</a></td><td class='p2DeleteThis'>X</td></tr></table></div>";
+	var newTimeTable = "<div class='p2TimeTableWrapper'><h1><strong>DAY NAME</strong> - 4.5h</h1><table><tr><td class='p2timeWorkingOn'>Client Work, Other Stuff, etc</td><td class='p2WhichClient'>Turner NBA Platform Build</td><td class='p2ProjectStatus'>Sprint 17</td><td class='p2TimeSpent'>1.25 h</td><td class='p2EditThis'><a href='#'>EDIT</a></td><td class='p2DeleteThis'>X</td></tr></table></div>";
 
 	$('#timesheet_grid').append( newTimeTable );
 
