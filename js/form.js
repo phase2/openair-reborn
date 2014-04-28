7b029a74302f6ac03c728539ae76f660262b16db
Jace Poirier-Pinto <jacepoirierpinto@gmail.com>
2014-04-28 18:04:10 -0400

    fixed submit button style

--- js/form.js
+++ js/form.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
   $rowTwo.append('<div class="p2_form-input_group p2_one-third"><label class="p2_form-label" for="p2_which_client">Which Client?</label><input type="text" id="p2_which_client" name"p2_which_client" class="p2_input-field" /></div>');
   $rowTwo.append('<div class="p2_form-input_group p2_one-third"><label class="p2_form-label" for="p2_which_task">Which Task?</label><input type="text" id="p2_which_task" name"p2_which_task" class="p2_input-field" /></div>');
-  $rowTwo.append('<div class="p2_form-input_group p2_one-third"><label class="p2_form-label" for="p2_how_long">How Long?</label><input type="text" id="p2_how_long" name"p2_how_long" class="p2_input-field" /><div class="p2_form-submit_button"><input type="submit" value="Start" /></div></div>');
+  $rowTwo.append('<div class="p2_form-input_group p2_one-third"><label class="p2_form-label" for="p2_how_long">How Long?</label><input type="text" id="p2_how_long" name"p2_how_long" class="p2_input-field" /><div class="p2_form-submit_button"><input type="submit" class="p2_submit" value="Start" /></div></div>');
 
   $form
     .append($row)
