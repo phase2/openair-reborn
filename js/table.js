27ec4b212fcd8c257842fb4c35a0ad1360ef781d
Jake Love <jlove@Jakes-MacBook-Pro.local>
2014-04-28 18:17:21 -0400

    Re-wrote long hand into function

--- js/table.js
+++ js/table.js
@@ -1,14 +1,10 @@
 $(function(){
 
-	var p2NewTimeTable = "<div class='p2TimeTableWrapper'><h1><strong>DAY NAME</strong> - 4.5h</h1><table><tr><td class='p2timeWorkingOn'>Client Work, Other Stuff, etc</td><td class='p2WhichClient'>Turner NBA Platform Build</td><td class='p2ProjectStatus'>Sprint 17</td><td class='p2TimeSpent'>1.25 h</td><td class='p2EditThis'><a href='#'>EDIT</a></td><td class='p2DeleteThis'>X</td></tr></table></div>";
-
-	$('#timesheet_grid').append( p2NewTimeTable );
-
 	var p2TimeEntries = [
 		{
 			'time': '0.5h',
 			'date': 'Mo',
-			'project': 'Project Name',
+			'project': 'Kick all other teams ass',
 			'task': 'Sprint 7',
 			'notes': 'Did some work on the stuff yanno',
 		},  
@@ -50,7 +46,7 @@ $(function(){
 		p2Sa = [],
 		p2Su = [];
 
-	$.each(p2TimeEntries, function(i, v){
+	$.each( p2TimeEntries, function(i, v){
 		if ( v.date == 'Mo' ){
 			p2Mo.push( v );
 		} else if ( v.date == "Tu" ){
@@ -68,4 +64,41 @@ $(function(){
 		}
 	});
 
+	var p2NewTimeTable = "<div class='p2TimeTableWrapper'><h1><strong>DAY NAME</strong> - <span>X.XX</span>h</h1></div>";
+
+	function p2BringTheAction ( arrayName, tableClass, dayName ){
+		if ( arrayName.length > 0 ){
+
+			var theSpecialSauce = tableClass;
+
+			$('#p2_content').append( $(p2NewTimeTable).addClass( tableClass ) );
+
+			$('.'+tableClass+' h1 strong').html( dayName );
+
+			var theNewTable = '<table>';
+			$.each( arrayName, function(i, v){
+				theNewTable = theNewTable + "<tr><td class='p2timeWorkingOn'>Client Work, Other Stuff, etc</td><td class='p2WhichClient'>"+v.project+"</td><td class='p2ProjectStatus'>Sprint 17</td><td class='p2TimeSpent'>1.25 h</td><td class='p2EditThis'><a href='#'>EDIT</a></td><td class='p2DeleteThis'>X</td></tr>";
+			});
+			theNewTable + '</table>';
+			$('.'+tableClass).append( theNewTable );
+
+		}
+	}
+
+	p2BringTheAction( p2Mo, 'newMondayTable', 'Monday' );
+	p2BringTheAction( p2Tu, 'newTuesdayTable', 'Tuesday' );
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
