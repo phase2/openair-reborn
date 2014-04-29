9b224dca84d661a411171b6905aa4918bb5b635b
Mike Crittenden <mikecrittenden@gmail.com>
2014-04-29 12:47:02 -0400

    make task field autocomplete work

--- js/form.js
+++ js/form.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
 
   $rowTwo.append('<div class="p2_form-input_group p2_which_client_wrapper"><label class="p2_form-label" for="p2_which_client">Which Client?</label><select id="p2_which_client" name="p2_which_client" class="p2_input-field" ></select></div>');
   $rowTwo.append('<div class="p2_form-input_group p2_which_task_wrapper"><label class="p2_form-label" for="p2_which_task">Which Task?</label><select id="p2_which_task" name="p2_which_task" class="p2_input-field"></select></div>');
-  $rowTwo.append('<div class="p2_form-input_group p2_when_wrapper"><label class="p2_form-label" for="p2_when">When?</label><input type="text" id="p2_when" name"p2_when" class="p2_input-field" /></div>');
   $rowTwo.append('<div class="p2_form-input_group p2_how_long_wrapper"><label class="p2_form-label" for="p2_how_long">How Long?</label><input type="text" id="p2_how_long" name"p2_how_long" class="p2_input-field" /></div>');
+  $rowTwo.append('<div class="p2_form-input_group p2_when_wrapper"><label class="p2_form-label" for="p2_when">When?</label><input type="text" id="p2_when" name"p2_when" class="p2_input-field" value="today" /></div>');
   $rowTwo.append('<div class="p2_form-submit_button"><input type="submit" class="p2_submit" value="Start" /></div>');
 
   $form
@@ -26,8 +26,33 @@ $(document).ready(function() {
     }
   });
 
+  $('#p2_how_long').on('input', function() {
+    if ($(this).val() == '') {
+      $('.p2_submit').val('Start');
+    } else {
+      $('.p2_submit').val('Add');
+    }
+  });
+
   $('#p2_which_client').chosen();
   $('#p2_which_task').chosen();
+
+  $('#p2_which_client').change(function() {
+    $('.timesheetControlPopupCustomerProject').last().val($('#p2_which_client').val());
+    var s = document.createElement('script');
+    s.textContent = "jQuery('.timesheetControlPopupCustomerProject').trigger('change')";
+    s.onload = function() {
+      this.parentNode.removeChild(this);
+    };
+    document.head.appendChild(s);
+    $('#p2_which_task').html();
+    $('.timesheetControlPopupCustomerProject').eq(-2).parent().next().find('option').each(function() {
+      if ($(this).text().length > 0) {
+        $('#p2_which_task').append('<option value="' + $(this).val() + '">' + $(this).text() + '</option>')
+      }
+    });
+    $("#p2_which_task").trigger("chosen:updated");
+  });
 
 
   $('.p2_submit').click(function(e) {
