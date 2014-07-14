e042c0a8b001342de3cdd441cb6b05984a14c294
Mike Crittenden <mikecrittenden@gmail.com>
2014-07-13 23:35:11 -0400

    Fetch tasks from existing task dropdowns if any exist for that project (huge performance bump)

--- app/scripts/openair.js
+++ app/scripts/openair.js
@@ -30,8 +30,7 @@ app.service('OpenAirService', function() {
     };
 
     /**
-     * Returns the list of available tasks for a given project
-     * in a key: value object.
+     * Returns the list of available tasks for a given project in a key: value object.
      *
      * @param projectId
      * @returns {Object} tasks
@@ -39,29 +38,53 @@ app.service('OpenAirService', function() {
     this.fetchTasks = function(projectId) {
         var tasks = {};
 
-        // Set the value of the last "Project" OA select (i.e., the empty one)
-        // to be the one we just chose, behind the scenes.
-        $('.timesheetControlPopupCustomerProject').last().val(projectId);
+        // First we need to see if a row already exists for this project,
+        // because if so, then we can just grab the tasks out of the "Tasks"
+        // dropdown that exists in that row already.
+        var $selects = [];
+        $selects = $('.timesheetControlPopupCustomerProject').filter(function() {
+            return $(this).val() === projectId;
+        });
 
-        // This is a horrible horrible hack. Chrome extensions don't have the
-        // ability to .trigger('change'); on an element, which is the only "easy"
-        // way to find the Tasks for a given Project. I.e., we need to trigger
-        // a change to the Project select so that the Tasks select after it
-        // will be populated, and we can grab those options. We can do that by
-        // building our own <script> DOM element and inserting it. :(
-        // Stolen/modified from http://stackoverflow.com/a/11388087, and yes
-        // I tried the .dispatchEvent() idea first, and it didn't work.
-        var s = document.createElement('script');
-        s.textContent = "jQuery('.timesheetControlPopupCustomerProject').trigger('change')";
-        s.onload = function () {
-            this.parentNode.removeChild(this);
-        };
-        document.head.appendChild(s);
+        if ($selects.length) {
+//            debugger;
+            // Yay! We already have a "Tasks" dropdown for that project, so we
+            // can just use that and move on.
+            var $taskoptions = $selects.first().parent().next().find('option');
 
-        // Now that the Tasks select input is populated, we can fetch the values out
-        // of it. However, There are no sane CSS selectors for that select input,
-        // so we end up with this monstrosity.
-        $('.timesheetControlPopupCustomerProject').eq(-2).parent().next().find('option').each(function () {
+        } else {
+//            debugger;
+            // Sadly, no rows already exist for this project, so we have to create
+            // one, and then manually trigger a .click() on the project select
+            // so that the task list gets built, and then cycle through that.
+            // This is not very performant, but I couldn't figure out how OA
+            // stores this stuff in JS, so this is the best I've got.
+
+            // Set the value of the last "Project" OA select (i.e., the empty one)
+            // to be the one we just chose, behind the scenes.
+            $('.timesheetControlPopupCustomerProject').last().val(projectId);
+
+            // This is a horrible horrible hack. Chrome extensions don't have the
+            // ability to .trigger('change'); on an element, which is the only "easy"
+            // way to find the Tasks for a given Project. I.e., we need to trigger
+            // a change to the Project select so that the Tasks select after it
+            // will be populated, and we can grab those options. We can do that by
+            // building our own <script> DOM element and inserting it. :(
+            // Stolen/modified from http://stackoverflow.com/a/11388087, and yes
+            // I tried the .dispatchEvent() idea first, and it didn't work.
+            var s = document.createElement('script');
+            s.textContent = "jQuery('.timesheetControlPopupCustomerProject').trigger('change')";
+            s.onload = function () {
+                this.parentNode.removeChild(this);
+            };
+            document.head.appendChild(s);
+
+            // Now that the Tasks select input is populated, we can fetch the values out
+            // of it. However, There are no sane CSS selectors for that select input,
+            // so we end up with this monstrosity.
+            var $taskoptions = $('.timesheetControlPopupCustomerProject').eq(-2).parent().next().find('option');
+        }
+        $taskoptions.each(function () {
             if ($(this).text().length > 0 && $(this).val() !== "0") {
                 var label = $(this).text().split(': ')[1];
                 tasks[$(this).val()] = label;
@@ -80,6 +103,7 @@ app.service('OpenAirService', function() {
      */
     this.parseTimesheet = function() {
         var timeEntries = [];
+        var projects = parent.fetchProjects();
 
         $('.timesheetHours').each(function () {
             var time = $(this).find('.timesheetInputHour').val();
@@ -89,7 +113,7 @@ app.service('OpenAirService', function() {
             }
             var date = $(this).find('a').attr('data-additional-title').substring(0, 2).toLowerCase();
             var project = $(this).parents('tr').find('.timesheetControlPopupCustomerProject').val();
-            var projectName = parent.fetchProjects()[project].split(' : ')[1];
+            var projectName = projects[project].split(' : ')[1];
             var task = $(this).parents('tr').find('.timesheetControlPopup').val();
             var taskName = $(this).parents('tr').find('.timesheetControlPopup option:selected').text().split(': ')[1];
             var notesID = $(this).find('a').attr('data-additional-prefix');
