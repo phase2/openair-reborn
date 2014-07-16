ab0db5dd8e6de1b0809aaca4f282d46ee23dd602
Mike Crittenden <mikecrittenden@gmail.com>
2014-07-16 12:12:42 -0400

    deletion confirmation, only submit if a chosen dropdown is not active, fix bug with deleting all copies of duplicate entries

--- app/scripts/timeentry.js
+++ app/scripts/timeentry.js
@@ -23,6 +23,12 @@ app.controller('TimeEntryController', ['$scope', 'OpenAirService', function($sco
     $scope.addTime = function(e) {
         e.preventDefault();
 
+        if (!$scope.notes || !$scope.project || !$scope.task || !$scope.time) {
+            // Don't submit time if there are some empty fields.
+            alert('Please fill out all fields before submitting the form.')
+            return;
+        }
+
         if ($scope.when === "" || typeof $scope.when === "undefined" || $scope.when.length === 0) {
             $scope.when = [$scope.getDay()];
         }
@@ -31,7 +37,7 @@ app.controller('TimeEntryController', ['$scope', 'OpenAirService', function($sco
             var timeEntry = {
                 time: $scope.time,
                 project: $scope.project,
-                projectName: OpenAirService.fetchProjects()[$scope.project],
+                projectName: OpenAirService.fetchProjects()[$scope.project].split(' : ')[1],
                 task: $scope.task,
                 taskName: OpenAirService.fetchTasks($scope.project)[$scope.task],
                 notes: $scope.notes,
@@ -82,12 +88,12 @@ app.controller('TimeEntryController', ['$scope', 'OpenAirService', function($sco
      * @param {String} day
      */
     $scope.deleteTime = function(entry, day) {
+        if (!confirm('Are you sure you want to delete that time entry?')) {
+            return;
+        }
         var entries = $scope.timeEntries[day];
         $scope.timeEntries[day] = entries.filter(function(element) {
-            if (element.time === entry.time &&
-                element.project === entry.project &&
-                element.task === entry.task &&
-                element.notes === entry.notes) {
+            if (element.id === entry.id) {
                 return false;
             }
             return true;
@@ -163,8 +169,25 @@ app.controller('TimeEntryController', ['$scope', 'OpenAirService', function($sco
      * @TODO: Use a directive instead.
      */
     document.addEventListener("keydown", function(e) {
-        var key = e.which || e.keyCode;
-        if (key === 13) {
+        if (e.keyIdentifier === "Enter") {
+
+            var $activeDropdowns = angular.element('.chosen-with-drop');
+            if ($activeDropdowns.length) {
+                // A chosen dropdown is active, so we may need to avoid submitting
+                // the form just yet, and instead, just let Chosen select the
+                // chosen option. Before we stop, though, we need to make sure it's
+                // not the "Day" multi-select OR, if it is, that the search field
+                // in there actually has a value. This weird hack exists because
+                // multi-selects in Chosen display a dropdown even if you don't
+                // type anything, and we only want to let it select a value in
+                // the dropdown if something was actually typed, otherwise just
+                // submit the form.
+                if (!angular.element('.chosen-container-multi').hasClass('chosen-with-drop') || // The "Day" field is NOT active, OR
+                    angular.element('.chosen-container-multi .search-field input').val() !== "") { // The "Day" field has a typed value
+                    // Don't submit the form.
+                    return;
+                }
+            }
 
             // Enter key was pressed! Can we submit the form?
             if ($scope.notes && $scope.project && $scope.task && $scope.time) {
